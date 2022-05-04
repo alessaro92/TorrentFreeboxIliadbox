77830a6e1dee65f452737a773ce978b140730eca
alessaro92 <alerex92@hotmail.it>
2022-05-04 15:11:19 +0200

    added localizations
    Added italian and english localizations with param for auth page

--- src/pages/config/config.ts
+++ src/pages/config/config.ts
@@ -16,10 +16,12 @@ export class ConfigPage {
 
   private authMessage:string;
   private subscriptionTimer:ISubscription;
+  private readonly brandModelParam:any;
 
   constructor(private navCtrl: NavController, public commonService: CommonService,
               public freeboxService: FreeboxService, public translate: TranslateService) {
       this.authMessage = "";
+      this.brandModelParam = { brandModel: commonService.isIliadbox() ? 'iliadbox' : 'Freebox' };
 
       translate.addLangs(['en', 'fr', 'it']);
       translate.setDefaultLang('en');
@@ -30,11 +32,13 @@ export class ConfigPage {
 
   authentification () {
       this.authMessage = "";
-      this.commonService.loadingShow("Demande en cours...");
+      const requestInProgress = this.translate.instant('pages.config.requestInProgress');;
+      this.commonService.loadingShow(requestInProgress);
       this.freeboxService.auth().then(auth => {
           this.commonService.loadingHide();
           if (auth) {
-              this.commonService.loadingShow("Veuillez autoriser l'application depuis la Freebox");
+              const pleaseAuthorize = this.translate.instant('pages.config.pleaseAuthorize', this.brandModelParam);
+              this.commonService.loadingShow(pleaseAuthorize);
               this.subscriptionTimer = Observable.interval(2500).subscribe(x => {
                   this.checkStatus();
               });
@@ -51,14 +55,16 @@ export class ConfigPage {
   checkStatus () {
       this.freeboxService.getStatus().then(status => {
           if (status=='granted') {
-              this.commonService.toastShow("Autorisation effectuée.");
+              const authorizationSuccessful = this.translate.instant('pages.config.authorizationSuccessful');
+              this.commonService.toastShow(authorizationSuccessful);
               this.commonService.loadingHide();
               this.subscriptionTimer.unsubscribe ();
               this.navCtrl.setRoot(TabsPage);
           } else if (status!='pending') {
+              const authorizationError = this.translate.instant('pages.config.authorizationError');
               this.subscriptionTimer.unsubscribe ();
               this.commonService.loadingHide();
-              this.commonService.toastShow("Erreur d'autorisation.");
+              this.commonService.toastShow(authorizationError);
           }
       });
   }
